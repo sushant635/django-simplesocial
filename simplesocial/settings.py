6f81833d36756d586372d3668e1cc7a260e19b94
sushant shinde <shindesushant9979@hotmail.com>
2020-12-06 22:51:39 +0530

    Database update

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'simplesocial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Demo_Test',
+        'USER':'postgres',
+        'PASSWORD':'sushant@123',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
